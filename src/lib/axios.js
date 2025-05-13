7b86fc2208c627f94f381554b24908ce04b3ae8d
Luong Minh Nhat <nhatluong.it@gmail.com>
2025-05-14 00:06:05 +0700

    update

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,10 +1,7 @@
 import axios from "axios";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const axiosInstance = axios.create({
-  baseURL: process.env.BACKEND_URL + "/api",
+  baseURL: "http://localhost:3000/api",
   withCredentials: true,
 });
 
